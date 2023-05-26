037cd41d17dec26d07afd08fbc129346ad923b1b
Sarita Kharabe <98185682+saritakharabe@users.noreply.github.com>
2023-05-26 10:07:16 -0400

    worked on Front End files queries and mutation.js and App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -25,6 +42,8 @@ function App() {
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
+
   );
 }
 
